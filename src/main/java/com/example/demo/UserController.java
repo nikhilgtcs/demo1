1036c1de1a8b0195ae6ddad9b302fbb521dd9140
iamnikhil096 <nikhilgautam0609@gmail.com>
2022-01-19 16:25:19 +0530

    made changes specified by parsuram

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -25,6 +25,6 @@ public class UserController {
 	@DeleteMapping("/user")
 	void deleteUser() {
 		// sdkjhd
-		// New line added to learn new Branch.
+		//removed previous comment, added new comment.		
 	}
 }
