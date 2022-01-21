2e26a10183b60f30e4c28f77e1103a95baa3fb77
iamnikhil096 <nikhilgautam0609@gmail.com>
2022-01-21 21:09:39 +0530

    Added database Integration

--- src/main/java/com/example/demo/service/IUserService.java
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -1,12 +1,17 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.UserRepository;
 import com.example.demo.entity.User;
 
+@Service
 public class IUserService {
-
+	@Autowired // Dependency Injection.
+	UserRepository userRepository;
 	public void saveUser(User user) {
-		// TODO Auto-generated method stub
-		
+		userRepository.save(user);
 	}
 	
 }
