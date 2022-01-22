b4daf6fc9acee4fadc15609b3258bbea9c4c23f1
iamnikhil096 <nikhilgautam0609@gmail.com>
2022-01-22 20:58:29 +0530

    added @Transactional annotation

--- src/main/java/com/example/demo/service/IUserService.java
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,12 @@ import com.example.demo.entity.User;
 public class IUserService {
 	@Autowired // Dependency Injection.
 	UserRepository userRepository;
+	
+	@Transactional //this annotation is used when we don't want any data to get added to database if any Exception or Error is faced.
+			//if we remove this annotation and try doing POST operation, then despite the error in line : 21 the data/record gets added to the database.
 	public void saveUser(User user) {
 		userRepository.save(user);
+		//throw new IllegalArgumentException();
 	}
 	public Iterable<User> getUsers() {
 		return userRepository.findAll();
@@ -21,6 +27,7 @@ public class IUserService {
 	public Optional<User> getUser(Integer id) {
 		return userRepository.findById(id);
 	}
+	@Transactional
 	public void deleteUser(Integer id) {
 		userRepository.deleteById(id);
 	}
